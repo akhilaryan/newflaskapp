ebf06cbf7091fe4bc9e926b92a710508b077ed31
akhilaryan <akhil.ion@gmail.com>
2014-08-25 12:39:32 +0100

    Improved form Validation

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,8 @@ from flask.ext.wtf import Form
 from wtforms import TextField, TextAreaField, SubmitField, validators
 
 class ContactForm(Form):
-	name = TextField("Name", [validators.Required()])
-	email = TextField("Email ID", [validators.Required(), validators.Email()])
-	subject = TextField("Subject", [validators.Required()])
-	message = TextAreaField("How can I help?", [validators.Required()])
+	name = TextField("Name", [validators.Required("Please enter your Name")])
+	email = TextField("Email ID", [validators.Required("Please enter a valid Email ID"), validators.Email()])
+	subject = TextField("Subject", [validators.Required("Please enter a Subject for your Message")])
+	message = TextAreaField("How can I help?", [validators.Required("Please enter your message for us.")])
 	submit = SubmitField("Send")
