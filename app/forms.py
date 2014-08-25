d78884b8b3486af43647e7818169c73552705141
akhilaryan <akhil.ion@gmail.com>
2014-08-25 12:22:25 +0100

    Basic form Validation Added

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask.ext.wtf import Form
-from wtforms.fields import TextField, TextAreaField, SubmitField
+from wtforms import TextField, TextAreaField, SubmitField, validators
 
 class ContactForm(Form):
-	name = TextField("Name")
-	email = TextField("Email ID")
-	subject = TextField("Subject")
-	message = TextAreaField("How can I help?")
+	name = TextField("Name", [validators.Required()])
+	email = TextField("Email ID", [validators.Required(), validators.Email()])
+	subject = TextField("Subject", [validators.Required()])
+	message = TextAreaField("How can I help?", [validators.Required()])
 	submit = SubmitField("Send")
